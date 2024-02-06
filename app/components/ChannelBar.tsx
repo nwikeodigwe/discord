8d9f48117555888e96d141aae05ee801b864cf0a
Nwike <nwikeodigwe@outlook.com>
2024-02-07 00:08:11 +0100

    Building collapsible categories with rotating icons

--- app/components/ChannelBar.tsx
+++ app/components/ChannelBar.tsx
@@ -1,8 +1,21 @@
 import * as Icon from "./icons";
 import Channels from "./Channels";
 import { data } from "../../data";
+import { useState } from "react";
 
 export default function ChannelBar() {
+  const [closedCategories, setClosedCategories] = useState<any>([]);
+
+  console.log(closedCategories);
+
+  const togglecategory = (categoryId: any) => {
+    setClosedCategories((closedCategories: string[]) => {
+      return closedCategories.includes(categoryId)
+        ? closedCategories.filter((id) => id !== categoryId)
+        : [...closedCategories, categoryId];
+    });
+  };
+
   return (
     <div className="bg-gray-800 w-60 flex flex-col">
       <div className="px-4 shadow-md flex items-center font-title font-semibold h-12 text-[15px] hover:bg-gray-550/[0.16] transition">
@@ -17,15 +30,29 @@ export default function ChannelBar() {
         {data[0].categories.map((category) => (
           <div key={category.id}>
             {category.label && (
-              <div className="px-1 font-title flex items-center uppercase text-xs tracking-wide">
-                <Icon.Arrow className="w-3 h-3" />
+              <div
+                onClick={() => togglecategory(category.id)}
+                className="px-1 font-title flex items-center uppercase text-xs tracking-wide hover:text-gray-100 w-full cursor-pointer"
+              >
+                <Icon.Arrow
+                  className={`${
+                    closedCategories.includes(category.id) ? "-rotate-90" : ""
+                  } w-3 h-3 transition duration-200`}
+                />
                 {category.label}
               </div>
             )}
             <div className="space-y-[0.5px] mt-[5px]">
-              {category.channels.map((channel) => (
-                <Channels channel={channel} key={channel.id} />
-              ))}
+              {category.channels
+                .filter((channel) => {
+                  const categoryIsOpen = !closedCategories.includes(
+                    category.id
+                  );
+                  return categoryIsOpen;
+                })
+                .map((channel) => (
+                  <Channels channel={channel} key={channel.id} />
+                ))}
             </div>
           </div>
         ))}
--- app/components/Channels.tsx
+++ app/components/Channels.tsx
@@ -8,6 +8,7 @@ interface Props {
     id: number;
     icon?: string;
     label: string;
+    unread?: boolean;
   };
 }
 
@@ -18,16 +19,28 @@ export default function Channels({ channel }: Props) {
       : Icons.Hashtag;
   const params = useParams<{ sid: string; cid: string }>();
   const active = +params.cid === +channel.id;
+  const state = active
+    ? "active"
+    : channel?.unread
+    ? "inactiveUnread"
+    : "inactiveRead";
+
+  const classes = {
+    active: "text-white bg-gray-550/[0.32]",
+    inactiveUnread:
+      "text-white hover:bg-gray-550/[0.16] active:bg-gray-550/[0.24]",
+    inactiveRead:
+      "text-gray-300 hover:text-gray-100 hover:bg-gray-550/[0.16]active:bg-gray-550/[0.24]",
+  };
   return (
     <Link href={`/servers/${params.sid}/channels/${channel.id}`}>
       <div
         key={channel.id}
-        className={`${
-          active === true
-            ? "text-white bg-gray-550/[0.32]"
-            : "text-gray-300 hover:text-gray-100 hover:bg-gray-550/[0.16]"
-        } flex items-center p-2 mx-2  hover:bg-gray-550/[0.16] transition-all rounded hover:text-gray-100 group`}
+        className={`${classes[state]} flex items-center p-2 mx-2  hover:bg-gray-550/[0.16] transition-all rounded hover:text-gray-100 group relative`}
       >
+        {state == "inactiveUnread" && (
+          <div className="absolute w-1 h-2 bg-white left-0 -ml-2 rounded-r-3xl"></div>
+        )}
         <Icon className="w-5 h-5 mr-1.5 text-gray-400" />
         {channel.label}
         <Icons.AddPerson className="w-4 h-4 text-gray-200 ml-auto opacity-0 group-hover:opacity-100" />
